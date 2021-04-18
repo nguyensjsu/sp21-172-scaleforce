dbd2705cd0494692198a2b41a1f739e686b139c2
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-18 04:08:00 -0700

    Get todos from backend

--- prototype/frontend/src/App.jsx
+++ prototype/frontend/src/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
 import './App.css';
 
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
+
+import TodoTable from './components/TodoTable';
+
 function App() {
+  const todos = useQuery('todos', async () => {
+    const response = await axios.get('/todos');
+    return response.data;
+  });
+
+  if (todos.isLoading) {
+    return (
+      <span>Loading...</span>
+    );
+  }
+
+  if (todos.isError) {
+    return (
+      <span>
+        Error:
+        {' '}
+        {todos.error.message}
+      </span>
+    );
+  }
+
   return (
-    <></>
+    <>
+      <TodoTable
+        initialTodos={todos.data}
+      />
+    </>
   );
 }
 
---
+++ prototype/frontend/src/components/TodoTable.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types,no-unused-vars */
+import React, { useState } from 'react';
+
+function TodoTable({ initialTodos }) {
+  const [todos, setTodos] = useState(initialTodos);
+
+  return (
+    <span>TodoTable placeholder</span>
+  );
+}
+
+export default TodoTable;
