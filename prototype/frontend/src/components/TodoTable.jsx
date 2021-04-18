c6aaeead0dfdd3da4eb886af3c728f07a35c49f1
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-18 04:30:47 -0700

    Display todos on frontend

--- prototype/frontend/src/components/TodoTable.jsx
+++ prototype/frontend/src/components/TodoTable.jsx
@@ -1,11 +1,48 @@
 /* eslint-disable react/prop-types,no-unused-vars */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+// TODO proptypes?
+
+const TodoRow = ({ todo }) => (
+  <tr>
+    <td>{todo.title}</td>
+    <td>{todo.description}</td>
+    <td>{todo.isCompleted ? 'Yes!' : 'No...'}</td>
+  </tr>
+);
 
 function TodoTable({ initialTodos }) {
   const [todos, setTodos] = useState(initialTodos);
+  const [todoRows, setTodoRows] = useState([]);
+
+  const initializeTodoRows = async () => {
+    setTodoRows(
+      todos.map((todo) => (
+        <TodoRow
+          key={todo.id}
+          todo={todo}
+        />
+      )),
+    );
+  };
+
+  useEffect(() => {
+    initializeTodoRows();
+  }, [todos]);
 
   return (
-    <span>TodoTable placeholder</span>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Completed?</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todoRows}
+      </tbody>
+    </table>
   );
 }
 
