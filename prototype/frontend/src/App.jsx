c7bc8a789a19f2a191a9e36fb819650234227ef8
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2021-04-20 01:30:09 -0700

    Implement adding new todos
    TODO query invalidation?

--- prototype/frontend/src/App.jsx
+++ prototype/frontend/src/App.jsx
@@ -2,36 +2,64 @@ import './App.css';
 
 import axios from 'axios';
 import React from 'react';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 
 import TodoTable from './components/TodoTable';
 
+const TodoForm = () => {
+  const mutation = useMutation((todo) => axios.post('/todos', todo));
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newTodo = {
+      title: formData.get('title'),
+      description: formData.get('description'),
+    };
+
+    mutation.mutate(newTodo);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="title">
+        <input type="text" name="title" placeholder="title" required />
+      </label>
+      <label htmlFor="description">
+        <input type="text" name="description" placeholder="description" required />
+      </label>
+      <input type="submit" />
+    </form>
+  );
+};
+
 function App() {
-  const todos = useQuery('todos', async () => {
+  const todosQuery = useQuery('todos', async () => {
     const response = await axios.get('/todos');
     return response.data;
   });
 
-  if (todos.isLoading) {
+  if (todosQuery.isLoading) {
     return (
       <span>Loading...</span>
     );
   }
 
-  if (todos.isError) {
+  if (todosQuery.isError) {
     return (
       <span>
         Error:
         {' '}
-        {todos.error.message}
+        {todosQuery.error.message}
       </span>
     );
   }
 
   return (
     <>
+      <TodoForm />
       <TodoTable
-        todos={todos.data}
+        todos={todosQuery.data}
       />
     </>
   );
