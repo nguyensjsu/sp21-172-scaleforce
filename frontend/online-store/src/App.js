a1c7f3b4f9f4bcbd43e165e8bdd3a1c88cb048d6
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-04-29 21:03:52 -0700

    Created Home.js for online-store and updated App.js

--- frontend/online-store/src/App.js
+++ frontend/online-store/src/App.js
@@ -8,6 +8,7 @@ import {
 import Navbar from './components/Navbar'
 import Signup from './screens/Auth/Signup'
 import Login from './screens/Auth/Login'
+import Home from "./screens/Home";
 
 export default function App() {
   return (
@@ -19,6 +20,9 @@ export default function App() {
         </Route>
         <Route path="/login">
           <Login/>
+        </Route>
+        <Route path="/">
+          <Home/>
         </Route>
       </Switch>
     </Router>
