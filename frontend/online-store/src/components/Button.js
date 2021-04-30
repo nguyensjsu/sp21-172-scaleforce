ada5e21d23d9285e603e59063bbe841dbd20833f
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-04-29 21:09:06 -0700

    Updated css for Button.js, Login.js, and Signup.js in online-store

--- frontend/online-store/src/components/Button.js
+++ frontend/online-store/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Button({ variant, label }) {
   return (
-    <button className="bg-blue text-white font-bold py-2 px-4 border-b-4 border-blue-dark rounded">
+    <button className="bg-gray-800 text-white font-bold py-2 px-4 border-b-4 border-blue-dark rounded">
       {label}
     </button>
   );
