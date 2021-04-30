c231f7e2c49406a8d304a2f184e82651d7d46b75
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-04-29 20:58:36 -0700

    Installed @headlessui/react and @heroicons/react and imported Navbar.js into cashier

--- frontend/cashier/src/App.js
+++ frontend/cashier/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './screens/Home/Home';
 import Login from './screens/Auth/Login';
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route path="/login">
           <Login />
