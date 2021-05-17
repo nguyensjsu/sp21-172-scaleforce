fa950c2162012df5fd4036afb92511c126d8210f
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-05-16 18:14:02 -0700

    Created card image and template for card number and card balance

--- frontend/online-store/src/screens/Card.js
+++ frontend/online-store/src/screens/Card.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import logo from '../components/card.jpg'
+import Table from '../components/Table'
+
+const data = [
+  {
+    col1: '156-875',
+    col2: '$20.00',
+  },
+];
+
+const columns = [
+  {
+    Header: 'Card Number',
+    accessor: 'col1'
+  },
+  {
+    Header: 'Balance',
+    accessor: 'col2',
+  },
+];
+
 const Card = () => {
   return (
     <div>
-      <h1>Temp</h1>
+      <img class="mx-auto p-5" src={logo} alt="card"/>
+      <Table data={data} columns={columns}/>
     </div>
   );
 };
