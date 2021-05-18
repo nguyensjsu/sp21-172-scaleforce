6ec4c0d4b7bea12b00b27e884e274cdc0d357082
jefuentesnava <jesusemmanuel.fuentesnava@sjsu.edu>
2021-05-18 16:03:45 -0700

    Updated login UI for all three apps and added auth service to  online-store

---
+++ frontend/online-store/src/services/auth.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const AUTH_URL = 'https://auth.scaleforce.dev/';
+
+export const login = async (email, password) => {
+  const res = await axios.post(AUTH_URL + 'auth', {
+    email,
+    password,
+  });
+
+  if (res.data) {
+    localStorage.setItem('user', JSON.stringify(res.data.jwt));
+  }
+
+  return res.data.uid;
+};
+
+export const validate = async () => {
+  try {
+    const token = JSON.parse(localStorage.getItem('user'));
+    const res = await axios.post(
+      AUTH_URL + 'validate',
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return res;
+  } catch (e) {
+    if (e.response.status === 500) {
+      logout();
+      console.log('LOGOUT');
+    }
+    return;
+  }
+};
+export const logout = () => {
+  localStorage.removeItem('user');
+};
+
+export const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem('user'));
+};
